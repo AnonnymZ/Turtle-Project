af3b2e8553432343cc68e9e7431dbb4d03809a7e
AnonnymZ <42273436+AnonnymZ@users.noreply.github.com>
2019-05-01 18:03:28 +0200

    Create fall_speed_num.py

---
+++ fall_speed_num.py
@@ -0,0 +1,46 @@
+from turtle import *
+import time
+import math
+bob = Turtle()
+i = 0
+nb = 0
+x = 0
+h = int(input())
+bob.home()
+bob.right(-90)
+bob.home()
+while i < h+10:
+    i = i + 1
+    bob.forward(10)
+    if h >= 50:
+        if i%10 == 0:
+            bob.write(i, font=("Arial", 8, "normal"))
+    else:  
+        if i%5 == 0:
+            bob.write(i, font=("Arial", 8, "normal"))
+bob.write("  M/S", font=("Arial", 15, "normal"))
+bob.home()
+bob.right(-90)
+while x < h+10:
+    x = x + 1
+    bob.forward(10)
+    if h >= 50:
+        if x%10 == 0:
+            bob.write(x, font=("Arial", 8, "normal"))
+    else:  
+        if x%5 == 0:
+            bob.write(x, font=("Arial", 8, "normal"))
+bob.write("  M", font=("Arial", 15, "normal"))
+bob.home()
+for w in range(0,h):
+    result = math.sqrt(9.8*2*w)
+    bob.goto(result*10,w*10)
+    bob.color("red")
+    if w%5 ==0:    
+        bob.write(math.ceil(result * 100) / 100, font=("Arial", 8, "normal"))
+bob.write(math.ceil(result * 100) / 100, font=("Arial", 8, "normal"))
+if h >= 50:
+    i = screensize()
+    print(i[0]+i[1])
+    screensize(i[0]+h*100/4,i[1]+h*100/4)
+    
